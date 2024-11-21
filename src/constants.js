4086b58e3ff17c187712f33221ca2447fcc8a8c7
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-11-21 14:04:06 +0200

    tech skills part done

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const TECH_SKILLS ={
+  'CSS_SELECTOR':{
+    'WRAP_CLASS': '.wrap',
+    'TECH_SKILLS_UPPER_ID': '#tech-skills-upper',
+    'TECH_SKILLS_LOWER_ID': '#tech-skills-lower',
+  },
+  'CSS_CLASS':{
+    'ITEM': 'item',
+    'UPPER_DOTS': 'upper-dots',
+    'LOWER_DOTS':'lower-dots',
+    'ITEM_UPPER':'item-upper',
+    'ITEM_LOWER':'item-lower',
+  },
+  'SEPARATOR_SVG_LOC': './img/icons.svg#icon-dot',
+  'PLACE':{
+    'UPPER': 'upper',
+    'LOWER': 'lower'
+  },
+  'POSITION':{
+    'BEFORE_END': "beforeend",
+    'AFTER_BEGIN': "afterbegin",
+    'AFTER_END': "afterend",
+    'BEFORE_BEGIN': "beforebegin",
+
+  }
+}
