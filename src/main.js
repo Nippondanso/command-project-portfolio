fa279cb90f088390e02dc51815a7a182201943d0
Andrej89Ko <78145272+Andrej89Ko@users.noreply.github.com>
2024-11-24 14:25:30 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './js/mobile-menu.js';
-import './js/tech-skills.js';
+import './js/tech-skills.html.js';
 import './js/questions-section.js';
 import './js/work-together.js';
 import './js/reviews.js';
